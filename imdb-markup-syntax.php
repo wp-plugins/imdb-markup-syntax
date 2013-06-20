a32ed2132026e027d8b9b7525580c5f9c499d3b9
HenrikRoos <HenrikRoos@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-20 03:45:51 +0000

    Adding first release 1.0
    git-svn-id: https://plugins.svn.wordpress.org/imdb-markup-syntax/trunk@728665 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ imdb-markup-syntax.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Main file for IMDb Markup Syntax WordPress Plugin
+ * 
+ * PHP version 5
+ * 
+ * @category  Runnable
+ * @package   Core
+ * @author    Henrik Roos <henrik.roos@afternoon.se>
+ * @copyright 2013 Henrik Roos
+ * @license   http://opensource.org/licenses/gpl-3.0.html GPL-3.0
+ * @link      https://github.com/HenrikRoos/imdb-markup-syntax imdb-markup-syntax
+ */
+require_once dirname(__FILE__) . "/Callback_Management.php";
+
+use IMDb_Markup_Syntax\Callback_Management;
+
+/**
+ * Plugin Name: IMDb Markup Syntax
+ * Plugin URI: https://github.com/HenrikRoos/imdb-markup-syntax
+ * Description: Add <strong>IMDb</strong> syntax functionallity in your post. Enter simple tags and this plugin replace with IMBb data direct from <a href="http://app.imdb.com">IMDb Mobile Applications</a>
+ * Version: 1.0
+ * Author: Henrik Roos
+ * Author URI: http://www.linkedin.com/pub/henrik-roos/28/148/348
+ * License: GPL-3.0
+ * License URI: http://opensource.org/licenses/gpl-3.0.html
+ */
+load_plugin_textdomain(
+    "imdb-markup-syntax", false, basename(dirname(__FILE__)) . "/languages"
+);
+
+$mgmt = new Callback_Management(get_locale());
+
+// Called by function prior to inserting into or updating the database.
+add_filter("wp_insert_post_data", array($mgmt, "filterImdbTags"), null, 2);
+
+//applied to the post title retrieved from the database, prior to printing on the
+//screen
+add_filter("the_title", array($mgmt, "liveFilterImdbTags"));
+
+//applied to the post content retrieved from the database, prior to printing on the
+//screen
+add_filter("the_content", array($mgmt, "liveFilterImdbTags"));
+?>
